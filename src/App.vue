99a1210f8b93a493ac07784c59145fb929b28135
Landy510 <randychen510@gmail.com>
2021-01-22 10:33:22 +0800

    上傳上課的練習-npm run build 有問題的版本

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <button class="btn btn-danger">按鈕</button>  
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created(){
+    this.$http.get('https://randomuser.me/api/').then((reponse) =>{
+      console.log(reponse.data);
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+</style>
